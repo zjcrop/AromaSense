798f360717300b5b139fc778d06b412515dc4aa3
zjcrop <127176814+zjcrop@users.noreply.github.com>
2026-08-24 21:33:47 +0800

    feat(android): add secure WebView application shell

---
+++ mobile/android/app/src/main/java/com/zjcrop/aromasense/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.zjcrop.aromasense
+
+import android.app.Activity
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : Activity() {
+    private lateinit var webView: WebView
+    private lateinit var database: android.database.sqlite.SQLiteDatabase
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        database = openOrCreateDatabase("aromasense.sqlite", MODE_PRIVATE, null)
+        database.execSQL("PRAGMA foreign_keys = ON")
+
+        webView = WebView(this)
+        webView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            allowFileAccess = true
+            allowContentAccess = false
+            mixedContentMode = WebSettings.MIXED_CONTENT_NEVER_ALLOW
+            cacheMode = WebSettings.LOAD_DEFAULT
+        }
+        WebView.setWebContentsDebuggingEnabled(BuildConfig.DEBUG)
+        webView.addJavascriptInterface(AromaSenseSQLiteBridge(database), "AromaSenseSQLite")
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, request: android.webkit.WebResourceRequest?): Boolean {
+                val url = request?.url ?: return true
+                return url.scheme != "file"
+            }
+        }
+        setContentView(webView)
+        webView.loadUrl("file:///android_asset/www/index.html")
+    }
+
+    override fun onBackPressed() {
+        if (::webView.isInitialized && webView.canGoBack()) webView.goBack() else super.onBackPressed()
+    }
+
+    override fun onDestroy() {
+        if (::webView.isInitialized) {
+            webView.removeJavascriptInterface("AromaSenseSQLite")
+            webView.destroy()
+        }
+        if (::database.isInitialized) database.close()
+        super.onDestroy()
+    }
+}
