0464437ff7f1adcf79e8facb3ce3664ff25fdce8
zjcrop <127176814+zjcrop@users.noreply.github.com>
2026-08-24 21:46:05 +0800

    fix(android): remove legacy Kotlin plugin DSL

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -24,10 +23,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
     }
 }
 
