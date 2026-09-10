c7814f6a62da5d4e15637ba0a2abe17d50f5ffae
zjcrop <127176814+zjcrop@users.noreply.github.com>
2026-09-11 00:53:15 +0800

    Wire adjacent node navigation hotfix

--- app/runtime/web-entry.ts
+++ app/runtime/web-entry.ts
@@ -20,6 +20,7 @@ import "../ui/dom/cupping-latest-ui-finalize";
 import "../ui/dom/cupping-input-ux-upgrade";
 import "../ui/dom/cupping-final-ux-20260910";
 import "../ui/dom/cupping-aroma-single-axis-score";
+import "../ui/dom/cupping-adjacent-navigation-hotfix";
 import { AromaSenseDomApp } from "./dom-app";
 import "./recognition-session-lifecycle";
 import { YingxiangBrowserBootstrap } from "./yingxiang-browser-bootstrap";
